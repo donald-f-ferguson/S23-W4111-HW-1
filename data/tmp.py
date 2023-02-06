6a4a61e7a65858ae3f8843f68a84798d75b432e8
donaldferguson <donff2@aol.com>
2023-02-06 14:59:03 -0500

    HW 1

---
+++ data/tmp.py
@@ -0,0 +1,9 @@
+def load_and_save_csv(data_dir:str, file_name:str, schema:str, table_name:str=None):
+    """
+
+    :param data_dir:
+    :param file_name:
+    :param schema:
+    :param table_name:
+    :return:
+    """
